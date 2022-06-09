0a8cd13255e7024dcb2f8e4831bfc4d08afd3c34
Doug Martin <doug@zoopdoop.com>
2022-06-09 08:55:17 -0400

    Working version with updated README

--- README.md
+++ README.md
@@ -2,12 +2,34 @@
 
 This enables mass export of all activities and sequences from LARA.
 
-This script is split into multiple steps where each previous step feeds into the next:
+## Setup
 
-The first step is invoked via: `npm run generate-csvs` which will generate a csv of all the activites and a csv of all the sequences.
+Before running this script it is recommended to use ssh port forwarding to connect to the LARA db through an ECS host as follows:
 
-The second step is invoked with `npm run export-json` which will export all the activies and sequences found in the csv files.
+ssh -L 127.0.0.1:3800:LARA-DB-HOSTNAME:3306 USERNAME@ECS-HOSTNAME
 
-Once exported the json files can be uploaded to S3 via this `aws` CLI invocation:
+for example:
 
-TODO
+ssh -L 127.0.0.1:3800:lara-staging-vpc.cheea3ib6y8u.us-east-1.rds.amazonaws.com:3306 dmartin@ec2-54-172-190-159.compute-1.amazonaws.com
+
+and then create a .env file (optionaly starting with the included `.env.sample` file) with the following contents:
+
+LARA_URL=      ; example: https://authoring.concord.org/
+LARA_SESSION=  ; the `_lightweight-standalone_session` cookie value (see below)
+DB_HOSTNAME=   ; example: localhost if using port forwarding or lara-vpc.cheea3ib6y8u.us-east-1.rds.amazonaws.com if direct connect
+DB_USERNAME=   ; example: master
+DB_PASSWORD=   ; example: nice-try-haxxors
+DB_PORT=       ; example: 3800 if using the port forwarding line above or 3306 if directly connecting
+WAIT_BETWEEN_REQUESTS= ; example: 1000 to wait 1 second between export requests
+
+The `_lightweight-standalone_session` cookie value can be copied from Chrome Developer Tools in the Application tab under the Cookies storage menu entry after you login as an admin to the LARA_URL specified in the .env file.  Make sure you check the "Show url decoded" checkbox before copying the value.  This cookie is needed as export is behind a permisssion check.
+
+## Usage
+
+This script is split into two steps where each previous step feeds into the next.
+
+The first step is invoked via: `npm run generate-csvs` which will generate a csv of all the activites and a csv of all the sequences.  The csv files will be placed in a subfolder named `data/DD-Mon-YYY`.
+
+The second step is invoked with `npm run export-json` which will export all the activies and sequences found in the csv files.  The json files will be placed in the same subfolder as the previous step.
+
+Once exported the json files can be uploaded to S3 via the `aws` CLI.  An upload step has not been added to the script as the CLI handles this well.
--- index.js
+++ index.js
@@ -1,12 +1,157 @@
+require('dotenv').config();
+
+const mysql = require('mysql2');
+const Papa = require("papaparse");
+const fs = require('fs');
+const request = require('request');
 const step = process.argv[2];
+
+const die = (message) => {
+  console.log(message);
+  process.exit(0);
+}
+
+const getEnvValue = (key) => {
+  if (process.env[key]) {
+    return process.env[key];
+  }
+  die(`Unknown ENV value: ${key}`);
+}
+
+const getEnv = () => {
+  const env = {
+    laraUrl: getEnvValue("LARA_URL"),
+    laraSession: getEnvValue("LARA_SESSION"),
+    dbHostname: getEnvValue("DB_HOSTNAME"),
+    dbUsername: getEnvValue("DB_USERNAME"),
+    dbPassword: getEnvValue("DB_PASSWORD"),
+    dbPort: getEnvValue("DB_PORT"),
+    waitBetweenRequests: getEnvValue("WAIT_BETWEEN_REQUESTS"),
+  };
+  env.waitBetweenRequests = parseInt(env.waitBetweenRequests)
+  if (isNaN(env.waitBetweenRequests)) {
+    env.waitBetweenRequests = 250
+  }
+  return env
+};
+
+const getPath = () => {
+  const d = (new Date()).toUTCString().replace(",", "").split(" ");
+  return `./data/${d[1]}-${d[2]}-${d[3]}`;
+}
+
+const mkdir = (path) => {
+  try {
+    fs.mkdirSync(path, { recursive: true });
+  } catch (e) {}
+}
+
+const generateCSVs = () => {
+  const env = getEnv();
+
+  const path = getPath();
+  mkdir(path);
+
+  const connection = mysql.createConnection({
+    host: env.dbHostname,
+    user: env.dbUsername,
+    port: env.dbPort,
+    password: env.dbPassword,
+    database: 'portal'
+  });
+
+  connection.promise()
+    .query('select la.id, la.name, u.email from lightweight_activities la, users u where la.user_id = u.id')
+    .catch(err => {
+      die(`Error getting activities: ${err.toString()}`);
+    })
+    .then(([rows, fields]) => {
+      fs.writeFileSync(`${path}/activities.csv`, Papa.unparse(rows, {delimiter: ",", header: true}));
+    })
+    .then(() => {
+      return connection.promise().query('select s.id, s.title, u.email from sequences s, users u where s.user_id = u.id');
+    })
+    .catch(err => {
+      die(`Error getting sequences: ${err.toString()}`);
+    })
+    .then(([rows, fields]) => {
+      fs.writeFileSync(`${path}/sequences.csv`, Papa.unparse(rows, {delimiter: ",", header: true}));
+    })
+    .then(() => connection.end());
+};
+
+const parseCSV = async (filename) => {
+  return new Promise((resolve) => {
+    Papa.parse(fs.createReadStream(filename), {
+      delimiter: ",",
+      header: true,
+      complete: (results) => resolve(results.data)
+    })
+  })
+}
+
+const getJSON = (env, type, row, rowNum, numRows) => {
+  return new Promise((resolve, reject) => {
+    const jar = request.jar();
+    const cookie = request.cookie(`_lightweight-standalone_session=${env.laraSession}`);
+    const url = `${env.laraUrl}/${type}/${row.id}/export`
+    console.log(`${rowNum} of ${numRows}: ${url}`)
+    jar.setCookie(cookie, url);
+    request({ url, jar }, (error, res, body) => {
+      if (error) {
+        reject(error)
+      } else if (res.statusCode == 200) {
+        resolve(body)
+      } else {
+        reject(`${res.statusCode}: ${url}`)
+      }
+    });
+  })
+}
+
+const awaitTimer = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), ms)
+  })
+}
+
+const exportJSON = () => {
+  const env = getEnv();
+  const path = getPath();
+
+  (async _ => {
+    for (const type of ["activities", "sequences"]) {
+      const typePath = `${path}/${type}`;
+      mkdir(typePath);
+
+      rows = await parseCSV(`${path}/${type}.csv`);
+      const numRows = rows.length;
+      let rowNum = 1;
+      for (let row of rows) {
+        try {
+          const startTime = Date.now();
+          const nextTime = startTime + env.waitBetweenRequests;
+          const json = await getJSON(env, type, row, rowNum, numRows);
+          const waitTime = Math.max(0, nextTime - Date.now())
+          const filename = `${typePath}/${row.id}.json`
+          fs.writeFileSync(filename, json);
+          await awaitTimer(waitTime)
+          rowNum++
+        } catch (e) {
+          console.log(e.toString())
+        }
+      }
+    }
+  })();
+};
 
 switch (step) {
   case "generate-csvs":
-    console.log("TODO: generate csvs step");
+    generateCSVs();
     break;
 
   case "export-json":
-    console.log("TODO: export json step");
+    exportJSON();
     break;
 
   case undefined:
